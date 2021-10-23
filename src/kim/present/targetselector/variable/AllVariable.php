0eb4fcf90f6fb056c7c4106e6779f9717e07b544
PresentKim <debe3721@gmail.com>
2021-10-23 11:57:15 +0900

    Change LICENSE to LGPLv3

--- src/kim/present/targetselector/variable/AllVariable.php
+++ src/kim/present/targetselector/variable/AllVariable.php
@@ -1,7 +1,6 @@
 <?php
 
-/*
- *
+/**
  *  ____                           _   _  ___
  * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
  * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
@@ -9,15 +8,23 @@
  * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * @author  PresentKim (debe3721@gmail.com)
  * @link    https://github.com/PresentKim
- * @license https://opensource.org/licenses/MIT MIT License
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
  *   (\ /)
  *  ( . .) ♥
  *  c(")(")
+ *
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpDocSignatureInspection
+ * @noinspection SpellCheckingInspection
+ * @noinspection PhpUnusedParameterInspection
+ * @noinspection PhpUnused
  */
 
 declare(strict_types=1);
